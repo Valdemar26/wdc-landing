fcc49d135507c098d9cdef0da01dab2ce54c6d53
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2020-08-31 21:34:16 +0300

    3: project skaffold, change components prefix to custom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderSliderComponent } from './components/header-slider/header-slider.component';
+import { WhatWeDoComponent } from './components/what-we-do/what-we-do.component';
+import { OurServicesComponent } from './components/our-services/our-services.component';
+import { StrategyComponent } from './components/strategy/strategy.component';
+import { RecentWorksComponent } from './components/recent-works/recent-works.component';
+import { OurValuesComponent } from './components/our-values/our-values.component';
+import { ClientsReviewsComponent } from './components/clients-reviews/clients-reviews.component';
+import { GetInTouchComponent } from './components/get-in-touch/get-in-touch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderSliderComponent,
+    WhatWeDoComponent,
+    OurServicesComponent,
+    StrategyComponent,
+    RecentWorksComponent,
+    OurValuesComponent,
+    ClientsReviewsComponent,
+    GetInTouchComponent
   ],
   imports: [
     BrowserModule,
