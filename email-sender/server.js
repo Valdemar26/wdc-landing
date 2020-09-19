0474f27ef8ef9d3ce1295f67ea8836628e0b9b70
Oleh <olehviznyi@gmail.com>
2020-09-19 19:22:38 +0300

    add button for blog, and contact form funtionality(not complete)

---
+++ email-sender/server.js
@@ -0,0 +1,63 @@
+let express = require("express"),
+  path = require('path'),
+  nodeMailer = require('nodemailer'),
+  bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(express.static('src'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post('/send-email', function (req, res) {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Імя клієнта: ${req.body.userName}</li>
+      <li>Електронна пошта ${req.body.userEmail}</li>
+      <li>повідомлення: ${req.body.message}</li>
+      <li>Телефон: ${req.body.userPhone}</li>
+    </ul>
+  `;
+
+
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+      user: '48130a1aaae514',
+      pass: '190265150c8baf'
+    }
+  });
+
+  let mailOptions = {
+    // should be replaced with real recipient's account
+    to: 'olehviznyi@gmail.com',
+    subject: req.body.subject,
+    body: req.body.message,
+    html: output // html body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+  // res.writeHead(302, { Location: 'http://localhost:4300' });
+  res.end();
+});
+
+let server = app.listen(8081, function(){
+  let port = server.address().port;
+  console.log("Server started at http://localhost:%s", port);
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -38,6 +39,7 @@ import { DeferLoadDirective } from './directives/defer-load.directive';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
