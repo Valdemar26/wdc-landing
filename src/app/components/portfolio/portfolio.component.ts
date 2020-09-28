0f187121b513ec19bf8805f88a5fa548d8d73df6
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2020-09-28 21:35:11 +0300

    50: create 'portfolio' markup, fixed bugs on 'recent-works'

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, HostBinding, OnDestroy, OnInit} from '@angular/core';
 import { Router} from '@angular/router';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {fromEvent, Subject} from 'rxjs';
+import {distinctUntilChanged, filter, map, pairwise, share, takeUntil, throttleTime} from 'rxjs/operators';
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
+}
 
 @Component({
   selector: 'itp-portfolio',
   templateUrl: './portfolio.component.html',
+  animations: [
+    trigger('toggle', [
+      state(
+        VisibilityState.Hidden,
+        style({ opacity: 0, transform: 'translateY(-100%)' })
+      ),
+      state(
+        VisibilityState.Visible,
+        style({ opacity: 1, transform: 'translateY(0)' })
+      ),
+      transition('* => *', animate('200ms ease-in'))
+    ])
+  ],
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  private ngUnsubscribe = new Subject();
+  private isVisible = true;
+
+  @HostBinding('@toggle')
+  get toggle(): VisibilityState {
+    return this.isVisible ? VisibilityState.Visible : VisibilityState.Hidden;
+  }
 
   constructor(private router: Router) { }
 
@@ -16,6 +50,39 @@ export class PortfolioComponent implements OnInit {
 
   public backToHome(): void {  // todo move to service
     this.router.navigate(['/main']);
+  }
+
+  public ngAfterViewInit(): void {
+    const scroll$ = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share(),
+      takeUntil(this.ngUnsubscribe)
+    );
+
+    const goingUp$ = scroll$.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const goingDown$ = scroll$.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    goingUp$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => (this.isVisible = true));
+
+    goingDown$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => (this.isVisible = false));
+  }
+
+  public ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
 }
